f5e4e16fdb0cf5f34ac3d4dff8d14a289dbe4481
Alasdair McLeay <alasdair.mcleay@me.com>
2020-04-01 16:50:02 +0100

    add basic TypeScript support

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import FlowComponent from './FlowComponent';
+import TypeScriptComponent from './TypeScriptComponent';
 
 function App() {
   return (
     <div className="App">
       <FlowComponent>Flow Component</FlowComponent>
+      <TypeScriptComponent>TypeScript Component</TypeScriptComponent>
     </div>
   );
 }
---
+++ src/TypeScriptComponent.tsx
@@ -0,0 +1,5 @@
+// @flow
+import React from 'react';
+import type { ReactNode } from 'react';
+
+export default ({children}: {children: ReactNode}) => <div>{children}</div>
